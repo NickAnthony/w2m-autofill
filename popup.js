16bb5ebcfcf1f87dc50d1b863c88e0fc40a43a9e
Nick Anthony <nicholas_anthony@brown.edu>
2018-02-04 10:46:45 -0500

    Small UI changes

--- popup.js
+++ popup.js
@@ -6,5 +6,17 @@ document.addEventListener('DOMContentLoaded',  () => {
                 console.log(response.text);
             });
         });
+
+        // Load the list of currently selected calendars
+        // var cal_list = document.getElementById('myCals');
+        // if (cal_list.childNodes.length > 0){
+        //     var myCals = JSON.parse(localStorage["myCals"]);
+        //     for(var i=0; i<myCals.length; i++){
+        //         var name = myCals[i].name;
+        //         var radioBtn = $('<input type="checkbox" name="calendarPicker" checked="true" id="' + i + '" /><label for="' + i + '">' + name + '</label> <br/>');
+        //         radioBtn.appendTo('#calList');
+        //         myCals[i].selected = true;
+        //     }
+        // }
     });
 });
