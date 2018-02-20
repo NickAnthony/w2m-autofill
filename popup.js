3e5b63e86a7b4380a6f1def52258a988cb7ada34
Nick Anthony <nicholas_anthony@brown.edu>
2018-02-20 14:48:34 -0500

    Signin button changes to 'you are signed in' after signing in. And the calendars are loaded into the popup window when the load button is pressed'

--- popup.js
+++ popup.js
@@ -3,16 +3,48 @@ document.addEventListener('DOMContentLoaded',  () => {
     load_button.addEventListener("click", function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {text: "loadGCalData"}, function(response) {
-                console.log(response.text);
+                loadCalendarsToPopup();
             });
         });
     });
 
     // This code is used to display all calendars within the popup window
     // Load the list of currently selected calendars
+    loadCalendarsToPopup();
+
+    // Initiallize local storage if it hasn't been initialized already
+    if (!localStorage["loggedIn"]) {
+        localStorage["loggedIn"] = "false";
+    }
+
+    // Handle Google Account Signin
+    var signin_button = document.getElementById("popupSignInBtn");
+    if (localStorage["loggedIn"] == "true") {
+        signin_button.innerHTML = "You are signed in.";
+    }
+
+    console.log("localStorage['loggedIn']", localStorage["loggedIn"]);
+
+    signin_button.addEventListener("click", function() {
+        chrome.runtime.sendMessage({text: "login"}, function(response) {
+            console.log(response);
+            if (response.text == "logged in!") {
+                localStorage["loggedIn"] = "true";
+                if (response.name) {
+                    signin_button.innerHTML = response.name;
+                }
+                else {
+                    signin_button.innerHTML = "You are signed in."
+                }
+            }
+        });
+    });
+});
+
+
+function loadCalendarsToPopup(){
     var cal_list = document.getElementById('calList');
-    console.log("cal_list.childNodes.length:", cal_list.childNodes.length);
-    if (cal_list.childNodes.length <= 3){
+    if (cal_list.childNodes.length <= 3 && localStorage["myCals"]){
         var myCals = JSON.parse(localStorage["myCals"]);
         for(var i=0; i<myCals.length; i++){
             if (myCals[i].selected) {
@@ -22,14 +54,5 @@ document.addEventListener('DOMContentLoaded',  () => {
             }
         }
     }
-
-    // Handle Google Account Signin
-    var signin_button = document.getElementById("popupSignInBtn");
-    signin_button.addEventListener("click", function() {
-        chrome.runtime.sendMessage({text: "login"}, function(response) {
-            console.log(response);
-        });
-
-    });
-});
+}
 
